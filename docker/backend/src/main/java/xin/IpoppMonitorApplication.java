b30670d645c7ec1867a430f690af5c925bd2857e
asurin <qiaozha@gmail.com>
2016-11-07 12:58:56 -0500

    problem with maven

---
+++ docker/backend/src/main/java/xin/IpoppMonitorApplication.java
@@ -0,0 +1,12 @@
+package xin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpoppMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpoppMonitorApplication.class, args);
+	}
+}
